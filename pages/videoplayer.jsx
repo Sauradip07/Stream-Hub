0ed476727d251e0a2ba60fb9079257a5b4fce514
Sauradip Ghosh <sauradip96ghosh@gmail.com>
2024-08-03 09:51:59 +0530

    S3 URL set

--- pages/videoplayer.jsx
+++ pages/videoplayer.jsx
@@ -29,14 +29,14 @@ const Room = () => {
                    controls={true}
                />
            </div>
-           {/* <div className='m-10'>
+           <div className='m-10'>
                <ReactPlayer
                    width="1280px"
                    height="720px"
-                   url="https://hhld-classes.s3.ap-south-1.amazonaws.com/Day+9.mp4"
+                   url="https://s3.eu-north-1.amazonaws.com/sauradipghosh.yt-test/y2mate.com+-+Shubh++King+Shit+Official+Audio_1080p.mp4"
                    controls={true}
                />
-           </div> */}
+           </div>
            <button type="button"
                onClick={callUser}
                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 m-10">Stream</button>
